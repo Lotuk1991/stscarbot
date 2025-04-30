4512826a2d379e53fd056adbddaa9b361ace9e45
Lotuk1991 <lotuk1991@gmail.com>
2025-04-30 19:05:38 +0200

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -37,14 +37,14 @@ def generate_import_pdf(breakdown, result, buffer, auction=None):
 
     # Основной блок
     for k, v in breakdown.items():
-        if k not in customs_keys and k not in additional_keys:
-    if k == "Рік випуску":
-        val = f"{v}"  # без доллара
-    elif isinstance(v, (int, float)):
-        val = f"${v:,.0f}"
-    else:
-        val = v
-    data.append([Paragraph(k, normal), Paragraph(val, normal)])
+    if k not in customs_keys and k not in additional_keys:
+        if k == "Рік випуску":
+            val = f"{v}"  # без доллара
+        elif isinstance(v, (int, float)):
+            val = f"${v:,.0f}"
+        else:
+            val = v
+        data.append([Paragraph(k, normal), Paragraph(val, normal)])
         if k == 'Рік випуску':
             # После года добавляем блок "Митні платежі"
             customs_present = [key for key in customs_keys if key in breakdown]
